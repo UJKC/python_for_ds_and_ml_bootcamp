7584d6042e04598b3c9ac6842883e5fbdaa9f9fb
Ujwal KC <ujwalkcsps@gmail.com>
2023-08-05 16:35:14 +0530

    numpy array indexing

--- numpy_tut/numpy_tut_1.py
+++ numpy_tut/numpy_tut_1.py
@@ -6,6 +6,10 @@ math1 = [[1, 2, 3, 4], [5, 6, 7, 8]]
 
 math2 = np.random.randint(0, 5, 10)
 
+math4 = math2[math2> 3]
+
+math3 = np.array(math1)
+
 print(np.array(math))
 print(np.array(math1))
 print(np.arange(start=0, stop=10))
@@ -21,3 +25,10 @@ print(math2.max())
 print(math2.argmax())
 print(math2.shape)
 print(math2.dtype)
+
+print(math2[0])
+print(math2[1: 5])
+print(math2[:5])
+print(math2[5:])
+print(math3[:1, 1:])
+print(math4)
