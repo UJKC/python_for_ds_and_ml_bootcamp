9cf90be84eebbf945ebe6c9e3532f3ba41cb44de
Ujwal KC <ujwalkcsps@gmail.com>
2023-08-05 20:47:29 +0530

    merge, join, concat pandas python code

---
+++ pandas_tut/pandas_tut_5.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+data1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3'],
+                        'C': ['C0', 'C1', 'C2', 'C3'],
+                        'D': ['D0', 'D1', 'D2', 'D3']},
+                        index=[0, 1, 2, 3])
+
+data2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
+                        'B': ['B4', 'B5', 'B6', 'B7'],
+                        'C': ['C4', 'C5', 'C6', 'C7'],
+                        'D': ['D4', 'D5', 'D6', 'D7']},
+                         index=[4, 5, 6, 7])
+
+data3 = pd.DataFrame({'A': ['A8', 'A9', 'A10', 'A11'],
+                        'B': ['B8', 'B9', 'B10', 'B11'],
+                        'C': ['C8', 'C9', 'C10', 'C11'],
+                        'D': ['D8', 'D9', 'D10', 'D11']},
+                        index=[8, 9, 10, 11])
+
+data4 = pd.concat([data1, data2, data3])
+
+data5 = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+
+data6 = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                          'C': ['C0', 'C1', 'C2', 'C3'],
+                          'D': ['D0', 'D1', 'D2', 'D3']})
+
+data7 = pd.merge(data5, data6, how= "inner", on= "key")
+
+data8 = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                     'key2': ['K0', 'K1', 'K0', 'K1'],
+                        'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3']})
+
+data9 = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                               'key2': ['K0', 'K0', 'K0', 'K0'],
+                                  'C': ['C0', 'C1', 'C2', 'C3'],
+                                  'D': ['D0', 'D1', 'D2', 'D3']})
+
+data10 = pd.merge(data8, data9, on=['key1', 'key2'])
+
+data11 = pd.DataFrame({'A': ['A0', 'A1', 'A2'],
+                     'B': ['B0', 'B1', 'B2']},
+                      index=['K0', 'K1', 'K2'])
+
+data12 = pd.DataFrame({'C': ['C0', 'C2', 'C3'],
+                    'D': ['D0', 'D2', 'D3']},
+                      index=['K0', 'K2', 'K3'])
+
+data13 = data11.join(data12)
+
+data14 = data12.join(data11)
+
+data15 = data12.join(data11, how= "outer")
+
+print(data1)
+print(data2)
+print(data3)
+print(data4)
+print(data5)
+print(data6)
+print(data7)
+print(data8)
+print(data9)
+print(data10)
+print(data11)
+print(data12)
+print(data13)
+print(data14)
+print(data15)
