012d67e6e87148b64000f3d048477b6a24767d22
Ujwal KC <ujwalkcsps@gmail.com>
2023-08-06 19:17:20 +0530

    tips data

--- seaborn_tut/seaborn_tut_1.py
+++ seaborn_tut/seaborn_tut_1.py
@@ -1,6 +1,15 @@
 import seaborn as sns
 
-print(sns.get_dataset_names())
-plot = sns.load_dataset('tips')
-plot.head()
-print(plot)
+#print(sns.get_dataset_names())
+#['anagrams', 'anscombe', 'attention', 'brain_networks', 'car_crashes', 'diamonds', 'dots', 'dowjones', 'exercise', 'flights', 'fmri', 'geyser', 'glue', 'healthexp', 'iris', 'mpg', 'penguins', 'planets', 'seaice', 'taxis', 'tips', 'titanic']
+
+#plot = sns.load_dataset('tips')
+#plot.head()
+#print(plot)
+
+import pandas as pd
+
+with open("seaborn_tut\\tips.csv") as data:
+    data_pd = pd.read_csv(data)
+
+print(data_pd)
