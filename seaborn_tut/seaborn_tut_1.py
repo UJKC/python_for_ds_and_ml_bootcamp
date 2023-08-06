bdb392ba9826d52bc8e38eb7664f3b647eb62558
Ujwal KC <ujwalkcsps@gmail.com>
2023-08-06 20:33:04 +0530

    distribution plot seaborn python code

--- seaborn_tut/seaborn_tut_1.py
+++ seaborn_tut/seaborn_tut_1.py
@@ -1,5 +1,5 @@
 import seaborn as sns
-
+import matplotlib.pyplot as mplpp
 #print(sns.get_dataset_names())
 #['anagrams', 'anscombe', 'attention', 'brain_networks', 'car_crashes', 'diamonds', 'dots', 'dowjones', 'exercise', 'flights', 'fmri', 'geyser', 'glue', 'healthexp', 'iris', 'mpg', 'penguins', 'planets', 'seaice', 'taxis', 'tips', 'titanic']
 
@@ -12,4 +12,40 @@ import pandas as pd
 with open("seaborn_tut\\tips.csv") as data:
     data_pd = pd.read_csv(data)
 
-print(data_pd)
+#print(data_pd)
+
+#1
+sns.distplot(data_pd['total_bill'])
+mplpp.show()
+
+#2
+sns.distplot(data_pd['total_bill'], kde= False)
+mplpp.show()
+
+#3
+sns.jointplot(x= 'total_bill', y= 'tip', data= data_pd)
+mplpp.show()
+
+#4
+sns.jointplot(x= 'total_bill', y= 'tip', data= data_pd, kind= 'hex')
+mplpp.show()
+
+#5
+sns.jointplot(x= 'total_bill', y= 'tip', data= data_pd, kind= 'reg')
+mplpp.show()
+
+#6
+sns.jointplot(x= 'total_bill', y= 'tip', data= data_pd, kind= 'kde')
+mplpp.show()
+
+#7
+sns.pairplot(data_pd)
+mplpp.show()
+
+#8
+sns.pairplot(data_pd, hue= 'sex')
+mplpp.show()
+
+#8
+sns.rugplot(data_pd['total_bill'])
+mplpp.show()
